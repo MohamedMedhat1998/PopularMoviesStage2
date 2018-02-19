5bb4f847b3b6a504befec3fa2ef6af9042b98bad
Abo_Med7at <abomed7at55@gmail.com>
2018-02-19 19:06:49 +0200

    Networking done but with fake api and fake data

--- app/src/main/java/com/andalus/abomed7at55/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/andalus/abomed7at55/popularmoviesstage1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.andalus.abomed7at55.popularmoviesstage1;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AsyncTask<Object,Object,String>(){
+
+
+            @Override
+            protected String doInBackground(Object... objects) {
+                NetworkingManager manager = new NetworkingManager();
+                try {
+                    manager.startConnection("https://api.myjson.com/bins/kap5l");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return manager.retrieveData();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Log.d("DATA",s);
+            }
+        }.execute();
     }
 
 }
---
+++ app/src/main/java/com/andalus/abomed7at55/popularmoviesstage1/NetworkingManager.java
@@ -0,0 +1,53 @@
+package com.andalus.abomed7at55.popularmoviesstage1;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+/**
+ * This class is used to set up the connection to the server to bring data
+ */
+
+public class NetworkingManager {
+
+    //The request method for httpUrlConnection
+    private static final String REQUEST_GET = "GET";
+    //This string holds the data coming from the connection
+    private String jsonData;
+    // TODO Create a builder for the real API
+    // TODO Access the real API
+    // TODO Change main Activity to be a custom list view or recyclerView
+    // TODO Display the posters in the main activity
+    /**
+     * Use this method to start the connection
+     * @param api the api that returns the json data
+     * @throws IOException if the api isn't valid
+     */
+    public void startConnection(String api) throws IOException {
+        //connecting
+        URL url = new URL(api);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod(REQUEST_GET);
+        urlConnection.connect();
+        //reading
+        InputStream in = urlConnection.getInputStream();
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(in);
+        Scanner scanner = new Scanner(bufferedInputStream);
+        scanner.useDelimiter("\\A");
+        jsonData = scanner.next();
+    }
+
+    /**
+     * This method is used to read the json data retrieved from the internet
+     * @throws NullPointerException if the {@link #startConnection(String)} method failed to make
+     * the connection
+     */
+    public String retrieveData() throws NullPointerException{
+        return jsonData;
+    }
+}
