d1f2683419145e2ae68a0e4d6e851e1797d4bf9b
Abo_Med7at <abomed7at55@gmail.com>
2018-02-21 18:49:28 +0200

    Data populated in the recyclerView but with UI bugs

--- app/src/main/java/com/andalus/abomed7at55/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/andalus/abomed7at55/popularmoviesstage1/MainActivity.java
@@ -3,18 +3,29 @@ package com.andalus.abomed7at55.popularmoviesstage1;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import org.json.JSONException;
 
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
         //ApiBuilder Test
         final String test = ApiBuilder.buildApi();
         //Log.d("api",test);
@@ -39,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
                 //Log.d("DATA",s);
                 try {
                     Movie[] movies = DataPicker.pickData(s);
+                    MovieAdapter adapter = new MovieAdapter(movies);
+                    RecyclerView.LayoutManager layoutManager
+                            = new GridLayoutManager(MainActivity.this,2, LinearLayoutManager.VERTICAL,false);
+
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setAdapter(adapter);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
---
+++ app/src/main/java/com/andalus/abomed7at55/popularmoviesstage1/MovieAdapter.java
@@ -0,0 +1,79 @@
+package com.andalus.abomed7at55.popularmoviesstage1;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.zip.Inflater;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * This Movie adapter is responsible for displaying movies in the recyclerView
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+
+    //The data
+    Movie[] movies;
+
+    Context mContext;
+
+    /**
+     * This constructor gives the adapter its initial data
+     * @param moviesArray the movies array that contains data
+     */
+    public MovieAdapter(Movie[] moviesArray){
+        movies = moviesArray;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        mContext = context;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item,parent,false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Picasso.with(mContext)
+                .load(movies[position].getPoster())
+                .error(android.R.drawable.stat_notify_error)
+                .into(holder.mainPoster);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.length;
+    }
+
+    /**
+     * This class represents the item in the recyclerView
+     */
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.iv_poster_in_list)
+        ImageView mainPoster;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+            mainPoster.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(),"DONE",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
